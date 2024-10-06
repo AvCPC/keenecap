d36002d87140b382dfa5987ae2f4d6b47e9b2457
AVC <dave@null>
2024-10-06 19:15:39 +0200

    add credslayer

--- keenecap/pcap_processor.py
+++ keenecap/pcap_processor.py
@@ -1,7 +1,14 @@
 from credslayer import process_pcap
-from keenecap.logger import logger
+from credslayer.core.utils import Credentials
 
-def process_pcap_with_logging(output_path):
+from keenecap.logger import logger
+def process_found_credentials(credentials: Credentials):
+    logger.info("Found:", credentials)
+    
+def process_pcap_with_logging(output_path, prefix='captures/'):
     logger.info(f"Starting pcap processing for {output_path}")
-    process_pcap(output_path)
+    try:
+        process_pcap(prefix+output_path)
+    except Exception as e:
+        logger.error(f"Error processing pcap: {e}") 
     logger.info(f"Completed pcap processing for {output_path}")
